13c3180ba64ef2ad2ef0f48d3f94456bf828bcb6
resadh <resadh@gmail.com>
2025-01-08 21:00:19 +0100

    Add project files.

---
+++ IMS.WebApp/Data/AccountDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace IMS.WebApp.Data
+{
+    public class AccountDbContext : IdentityDbContext
+    {
+        public AccountDbContext(DbContextOptions<AccountDbContext> options) : base(options)
+        {
+            
+        }
+
+    }
+}
