15e1926f69aecb4bbb31778fb499fc1ed9c0bb8f
resadh <44710466+resadh@users.noreply.github.com>
2025-01-09 23:56:33 +0100

    Delete IronForge.UseCases directory

--- IronForge.UseCases/Inventories/Interfaces/IViewInventoriesByNameUseCase.cs
+++
@@ -1,9 +0,0 @@
-﻿using IronForge.CoreBusiness;
-
-namespace IronForge.UseCases.Inventories.Interfaces
-{
-    public interface IViewInventoriesByNameUseCase
-    {
-        Task<IEnumerable<Inventory>> ExecuteAsync(string name = " ");
-    }
-}
